f9f126c6e48bcc5d816626cda2e61e34047f4846
Oracle Public Cloud User <opc@minecraft-server-482064.subnet01251703.vcn01251703.oraclevcn.com>
2023-01-29 22:54:10 +0000

    allow manually specifying key

--- s3_backup.py
+++ s3_backup.py
@@ -11,12 +11,13 @@ def find_backup_file(backups_dir, file_ext=".zip"):
     files = [f"{backups_dir}/{file}" for file in os.listdir(backups_dir) if file.endswith(file_ext)]
     return max(files, key=os.path.getctime)
 
-def backup_world(bucket, backup_filename):
+def backup_world(bucket, backup_filename, key=None):
     with open(backup_filename, "rb") as data:
         try:
-            key = backup_filename.split("/")[-1]
+            if not key:
+                key = backup_filename.split("/")[-1]
             logging.info(f"Uploading world archive {key} to {bucket.name}...")
-            archive = bucket.upload_fileobj(data, key)
+            bucket.upload_fileobj(data, key)
         except Exception as e:
             logging.error(f"Failed to archive: {e}")
 
@@ -24,6 +25,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="s3_backup", description="backups a world archive to a s3 bucket")
     parser.add_argument("-d", "--backup-dir", required=True, help="directory containing local world archives (backups)")
     parser.add_argument("-b", "--bucket", required=True, help="s3 bucket name")
+    parser.add_argument("-k", "--key", help="The key to save the backup object as")
 
     args = parser.parse_args()
     print(args.backup_dir, args.bucket) 
@@ -34,4 +36,4 @@ if __name__ == "__main__":
     if not backup_file:
         logger.error(f"No files to backup found in {backups_dir}")
         exit()
-    backup_world(bucket=bucket, backup_filename=backup_file)
+    backup_world(bucket=bucket, backup_filename=backup_file, key=args.key)
